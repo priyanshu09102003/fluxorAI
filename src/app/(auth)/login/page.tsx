c945019b4786fe5a4f8f5603e6e4a68458c5501f
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-01-12 01:30:29 +0530

    Fixing the prisma v7 usage with Prisma Adapter and basic development of the Login Form using zod, react hook forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "@/features/auth/components/login-form";
+
 const Page = () => {
     return(
         <div>
-            Login
+            <LoginForm/>
         </div>
     )
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,14 @@
-import {PrismaClient} from "../generated/prisma"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if(process.env.NODE_ENV !== "production"){
     globalForPrisma.prisma = prisma

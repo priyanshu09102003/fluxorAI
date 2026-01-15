6222c501678a5423875ca4dc14f6468a3357ccd7
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-01-16 03:39:09 +0530

    Setting up Polar, Polar sdk and connecting polar with Better-auth to create customer on signup and sync with database

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,36 @@
+import {checkout, polar, portal} from "@polar-sh/better-auth"
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db"
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql"
     }),
+
     emailAndPassword:{
         enabled: true,
         autoSignIn: true
-    }
+    },
+
+    plugins:[
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products:[
+                        {
+                            productId: "ea091938-0c40-4a12-85d0-1622ba8c2e88",
+                            slug: "FluxorAI-PRO"
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ]
+        })
+    ]
 })

f7d8abec5d8d6bf20bc0e2cd58a7c5a99ad67eb4
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-02-01 00:36:38 +0530

    Making the crdentials trpc router and regoistering it in _apps.ts

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,9 +3,11 @@ import { inngest } from '@/inngest/client';
 import {createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { workFlowsRouter } from '@/features/workflows/server/routers';
+import { credentialsRouter } from '@/features/credentials/server/routers';
 
 export const appRouter = createTRPCRouter({
-  workflows: workFlowsRouter
+  workflows: workFlowsRouter,
+  credentials: credentialsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
